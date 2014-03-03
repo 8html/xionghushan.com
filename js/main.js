38f84195271c7106e2c931cf6661289837eefbe4
caiguanhao <caiguanhao@gmail.com>
2014-03-03 20:18:53 +0800

    add filter;

--- js/main.js
+++ js/main.js
@@ -7,4 +7,26 @@ $(function() {
     panelfx: "sliding",
     panelfxspeed: 1000
   });
+  $('#finder').isotope({
+    itemSelector: '.item'
+  });
+  var update_filter = function() {
+    var filter;
+    filter = '';
+    $('#finder-filter .panel').each(function(a, b) {
+      var active;
+      active = $(b).find('a.active:first');
+      filter += active.data('filter');
+      $(b).find('span.value').text(active.text());
+    });
+    $('#finder').isotope({
+      filter: filter
+    });
+  };
+  $('#finder-filter a').click(function(e) {
+    e.preventDefault();
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    update_filter();
+  });
 });
